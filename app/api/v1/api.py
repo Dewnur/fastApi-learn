b3b11eece67bf3b1864b06b1e39e03cedd2cf249
denis-kosobanov <Kosobanovden@mail.ru>
2023-10-20 00:57:06 +0500

    add auth router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
+from app.api.v1.endpoints import (
+    auth
+)
 
 api_router = APIRouter()
+
+api_router.include_router(auth.router, prefix='/auth', tags=['Аутентификация и Авторизация'])
