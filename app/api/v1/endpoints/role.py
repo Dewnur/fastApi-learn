1b61feb54531a98715eeb7f09d301f88a8812c81
denis-kosobanov <kosobanovden@mail.ru>
2023-10-30 00:35:31 +0500

    Refactor role

--- app/api/v1/endpoints/role.py
+++ app/api/v1/endpoints/role.py
@@ -2,23 +2,27 @@ from fastapi import APIRouter, Depends
 
 from app import crud
 from app.api.deps import get_current_user, model_id_existing
-from app.models import User, Role
+from app.models import Role
 from app.schemas.role_schema import IRoleEnum, IRoleRead
 
 router = APIRouter()
 
 
-@router.get('/{obj_id}')
+@router.get(
+    '/{obj_id}',
+    dependencies=[Depends(get_current_user([IRoleEnum.admin]))]
+)
 async def get_role_by_id(
         role_by_id: IRoleRead = Depends(model_id_existing(Role)),
-        current_user: User = Depends(get_current_user([IRoleEnum.admin]))
 ) -> IRoleRead:
     return role_by_id
 
 
-@router.get('')
+@router.get(
+    '',
+    dependencies=[Depends(get_current_user([IRoleEnum.admin]))]
+)
 async def get_role_list(
-        current_user: User = Depends(get_current_user([IRoleEnum.admin]))
 ) -> list[IRoleRead]:
     role_list = await crud.role.fetch_all()
     return role_list
