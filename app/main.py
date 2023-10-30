aebfa33bddc8e4ae16b8b0eae53e5b969517bd5c
denis-kosobanov <kosobanovden@mail.ru>
2023-10-31 01:42:09 +0500

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi_async_sqlalchemy import SQLAlchemyMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 from app.core.config import get_settings
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 app.add_middleware(
     SQLAlchemyMiddleware,
