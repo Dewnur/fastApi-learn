56bd14c153d715edd190f14d520e074d7b825717
denis-kosobanov <kosobanovden@mail.ru>
2023-11-05 16:13:37 +0500

    Update router_base

--- app/api/router_base.py
+++ app/api/router_base.py
@@ -1,4 +1,5 @@
-from typing import TypeVar, Sequence
+from enum import Enum
+from typing import TypeVar, Generic, Sequence
 
 from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel
@@ -13,92 +14,140 @@ SchemaUpdateType = TypeVar("SchemaUpdateType", bound=BaseModel)
 ModelType = TypeVar("ModelType", bound=Base)
 
 
-class RouteDepends:
-    def __init__(
-            self,
-            *,
-            get_route: Sequence[Depends] | None = None,
-            get_list_route: Sequence[Depends] | None = None,
-            post_route: Sequence[Depends] | None = None,
-            update_route: Sequence[Depends] | None = None,
-            delete_route: Sequence[Depends] | None = None,
-    ) -> None:
-        self.get_route = get_route
-        self.get_list_route = get_list_route
-        self.post_route = post_route
-        self.update_route = update_route
-        self.delete_route = delete_route
+class BaseAPIMethods(str, Enum):
+    get = 'get'
+    get_list = 'get_list'
+    post = 'post'
+    put = 'put'
+    delete = 'delete'
 
 
-class BaseAPIRouter(APIRouter):
+class BaseAPIRouter(
+    APIRouter,
+    Generic[
+        ModelType,
+        SchemaCreateType,
+        SchemaReadType,
+        SchemaUpdateType
+    ]
+):
     def __init__(
             self,
-            *,
             model: type[ModelType],
-            model_schema_read: type[SchemaReadType],
-            model_schema_create: type[SchemaCreateType],
-            model_schema_update: type[SchemaUpdateType],
-            route_depends: RouteDepends,
+            schema_create_type: type[SchemaCreateType],
+            schema_read_type: type[SchemaReadType],
+            schema_update_type: type[SchemaUpdateType],
+            depends_mapping: dict[str | BaseAPIMethods, Sequence[Depends]] = None,
     ):
         super().__init__()
+        self.schema_read_type = schema_read_type
+        self.schema_create_type = schema_create_type
+        self.schema_update_type = schema_update_type
+        self.model = model
+        self.depends_mapping = depends_mapping or {}
 
-        @self.get(
+        self.add_api_route(
             path='/{obj_id}',
-            dependencies=route_depends.get_route,
+            methods=['GET'],
+            endpoint=self._get(),
+            dependencies=self.depends_mapping.get(BaseAPIMethods.get, None),
             status_code=status.HTTP_200_OK,
             name=f'Get {model.__name__} By Id',
-
         )
-        async def get_object_by_id(
-                object_by_id: model_schema_read = Depends(model_id_existing(model))
-        ) -> model_schema_read:
-            return object_by_id
 
-        @self.get(
+        self.add_api_route(
             path='',
-            dependencies=route_depends.get_list_route,
+            methods=['GET'],
+            endpoint=self._get_list(),
+            dependencies=self.depends_mapping.get(BaseAPIMethods.get_list, None),
             status_code=status.HTTP_200_OK,
             name=f'Get {model.__name__} List',
         )
-        async def get_object_list(
-        ) -> list[model_schema_read]:
-            model_crud = CRUDBase(model)
-            object_list: model_schema_read = await model_crud.fetch_all()
-            return object_list
 
-        @self.post(
+        self.add_api_route(
             path='',
-            dependencies=route_depends.post_route,
+            methods=['POST'],
+            endpoint=self._post_object(),
+            dependencies=self.depends_mapping.get(BaseAPIMethods.post, None),
             status_code=status.HTTP_201_CREATED,
             name=f'Create {model.__name__}',
         )
+
+        self.add_api_route(
+            path='/{obj_id}',
+            methods=['PUT'],
+            endpoint=self._put_object(),
+            dependencies=self.depends_mapping.get(BaseAPIMethods.put, None),
+            status_code=status.HTTP_200_OK,
+            name=f'Update {model.__name__}',
+        )
+
+        self.add_api_route(
+            path='/{obj_id}',
+            methods=['DELETE'],
+            endpoint=self._delete_object(),
+            dependencies=self.depends_mapping.get(BaseAPIMethods.delete, None),
+            status_code=status.HTTP_200_OK,
+            name=f'Delete {model.__name__}',
+        )
+
+    def _get(self):
+        schema_read_type = self.schema_read_type
+        model = self.model
+
         async def get_object_by_id(
-                obj: model_schema_create
+                object_by_id: schema_read_type = Depends(model_id_existing(model)),
+        ) -> schema_read_type:
+            return object_by_id
+
+        return get_object_by_id
+
+    def _get_list(self):
+        schema_read_type = self.schema_read_type
+        model = self.model
+
+        async def get_object_list(
+        ) -> list[schema_read_type]:
+            model_crud = CRUDBase(model)
+            object_list = await model_crud.fetch_all()
+            return object_list
+
+        return get_object_list
+
+    def _post_object(self):
+        schema_create_type = self.schema_create_type
+        model = self.model
+
+        async def post_object(
+                obj: schema_create_type
         ) -> None:
             model_crud = CRUDBase(model)
             return await model_crud.create(obj=obj)
 
-        @self.put(
-            '/{obj_id}',
-            dependencies=route_depends.update_route,
-            status_code=status.HTTP_200_OK,
-            name=f'Update {model.__name__}',
-        )
+        return post_object
+
+    def _put_object(self):
+        schema_update_type = self.schema_update_type
+        schema_read_type = self.schema_read_type
+        model = self.model
+
         async def update_object(
-                obj: model_schema_update,
-                object_by_id: model_schema_read = Depends(model_id_existing(model))
+                obj: schema_update_type,
+                object_by_id: schema_read_type = Depends(model_id_existing(model))
         ) -> None:
             model_crud = CRUDBase(model)
             await model_crud.update(obj_current=object_by_id, obj_new=obj)
 
-        @self.delete(
-            '/{obj_id}',
-            dependencies=route_depends.delete_route,
-            status_code=status.HTTP_200_OK,
-            name=f'Delete {model.__name__}',
-        )
+        return update_object
+
+    def _delete_object(self):
+        schema_read_type = self.schema_read_type
+        model = self.model
+
         async def delete_object(
-                object_by_id: model_schema_read = Depends(model_id_existing(model))
-        ) -> model_schema_read:
+                object_by_id: schema_read_type = Depends(model_id_existing(model))
+        ) -> schema_read_type:
             model_crud = CRUDBase(model)
             await model_crud.delete(id=object_by_id.id)
+
+        return delete_object
