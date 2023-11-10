ef535b08817401be8b7b9e4ae8366f6f59c22d2f
denis-kosobanov <kosobanovden@mail.ru>
2023-11-10 06:08:29 +0500

    Delete create order in CRUDOrder, update post order

--- app/api/v1/endpoints/profile.py
+++ app/api/v1/endpoints/profile.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud
-from app.api.deps import get_current_user, model_id_existing
+from app.api.deps import get_current_user, get_db_session
+from app.dependencies.order_deps import order_id_existing
+from app.dependencies.profile_deps import validate_user_profile, profile_id_existing
 from app.models import User, Profile
-from app.schemas import IOrderStatus
 from app.schemas.order_items_schema import IOrderItemCreate
-from app.schemas.order_schema import IOrderRead, IOrderCreate
+from app.schemas.order_schema import IOrderRead
 from app.schemas.profile_schema import IProfileRead, IProfileUpdate
 from app.schemas.role_schema import IRoleEnum
 from app.schemas.user_schema import IUserRead
+from app.services.order_service import create_order
 
 router = APIRouter()
 
@@ -37,41 +40,29 @@ async def update_profile(
 
 
 @router.post(
-    path='/{obj_id}/orders',
+    path='/{profile_id}/orders',
     status_code=status.HTTP_201_CREATED
 )
 async def post_order(
         order_items: list[IOrderItemCreate],
         current_user: User = Depends(get_current_user([IRoleEnum.user])),
-        profile_by_id: Profile = Depends(model_id_existing(Profile))
+        profile_by_id: Profile = Depends(profile_id_existing),
+        db_session: AsyncSession = Depends(get_db_session)
 ) -> IOrderRead:
-    if not order_items:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail='Order items list is empty'
-        )
-
-    profile: Profile = current_user.profile
-    if not profile.id == profile_by_id.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Invalid token"
-        )
-    if not profile.phone_number:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail='No phone number provided'
-        )
-    if not profile.address:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail='Address not specified'
-        )
-    return await crud.order.create(
-        order=IOrderCreate(
-            profile_id=profile.id,
-            order_items=order_items,
-            status=IOrderStatus.processing,
-            total_amount=0,
-        )
+    validate_user_profile(current_user.profile, profile_by_id)
+    return await create_order(
+        profile_id=profile_by_id.id, order_items=order_items, db_session=db_session
     )
+
+
+@router.get(
+    path='/{profile_id}/orders/{order_id}',
+    status_code=status.HTTP_200_OK
+)
+async def get_profile_order(
+        profile_by_id: Profile = Depends(profile_id_existing),
+        order_by_id: IOrderRead = Depends(order_id_existing),
+        current_user: User = Depends(get_current_user([IRoleEnum.user])),
+) -> IOrderRead:
+    validate_user_profile(current_user.profile, profile_by_id)
+    return order_by_id
--- app/crud/order_crud.py
+++ app/crud/order_crud.py
@@ -1,64 +1,9 @@
-from fastapi import HTTPException, status
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app import crud
 from app.crud.crud_base import CRUDBase
-from app.models import Order, OrderItem, Product
-from app.schemas.order_schema import IOrderCreate
-from app.utils.exceptions.common_exception import IdNotFoundException
+from app.models import Order
 
 
 class CRUDOrder(CRUDBase[Order]):
-    async def create(
-            self,
-            *,
-            order: IOrderCreate,
-            db_session: AsyncSession | None = None,
-    ) -> Order:
-        db_session = db_session or self.get_db().session
-        try:
-            new_order = Order(**order.model_dump(exclude={'order_items'}))
-            db_session.add(new_order)
-            await db_session.flush()
-
-            for item in order.order_items:
-                product = await crud.product.fetch_one(id=item.product_id)
-                if not product:
-                    raise IdNotFoundException(
-                        model=Product,
-                        id=item.product_id
-                    )
-                if product.stock_quantity <= 0 or product.stock_quantity < item.quantity:
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail="Product is unavailable due to insufficient stock quantity"
-                    )
-                if item.quantity <= 0:
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail='The quantity must be greater than 0'
-                    )
-                product.stock_quantity -= item.quantity
-                new_item = OrderItem(
-                    product_id=product.id,
-                    order_id=new_order.id,
-                    quantity=item.quantity,
-                    unit_price=product.price,
-                )
-                new_order.total_amount += product.price * item.quantity
-                db_session.add(new_item)
-                db_session.add(product)
-
-            await db_session.commit()
-            await db_session.refresh(new_order)
-            return new_order
-        except SQLAlchemyError as e:
-            await db_session.rollback()
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=str(e)
-            )
+    pass
 
 
 order = CRUDOrder(Order)
