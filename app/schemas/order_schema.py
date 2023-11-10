a7d5f2313bf7a9dd97d4d071b01b32db32918d56
denis-kosobanov <kosobanovden@mail.ru>
2023-11-10 06:05:56 +0500

    Update order schema

--- app/schemas/order_schema.py
+++ app/schemas/order_schema.py
@@ -4,13 +4,12 @@ from uuid import UUID
 from pydantic import BaseModel
 
 from app.schemas import IOrderStatus
-from app.schemas.order_items_schema import IOrderItemCreate
+from app.schemas.order_items_schema import IOrderItemCreate, IOrderItemRead
 
 
 class IOrderBase(BaseModel):
-    order_items: list[IOrderItemCreate]
-    status: IOrderStatus
-    total_amount: float
+    status: IOrderStatus = IOrderStatus.processing
+    total_amount: float = 0.0
 
     class ConfigDict:
         from_attributes = True
@@ -18,6 +17,7 @@ class IOrderBase(BaseModel):
 
 class IOrderCreate(IOrderBase):
     profile_id: UUID
+    order_items: list[IOrderItemCreate]
 
 
 class IOrderRead(IOrderBase):
---
+++ app/services/order_service.py
@@ -0,0 +1,61 @@
+from uuid import UUID
+
+from fastapi import HTTPException, status
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import crud
+from app.models import Order, Product, OrderItem
+from app.schemas import IOrderStatus
+from app.schemas.order_items_schema import IOrderItemCreate
+from app.schemas.order_schema import IOrderRead
+from app.utils.exceptions.common_exception import IdNotFoundException
+from app.utils.exceptions.order_items_excemption import EmptyOrderItemsException
+from app.utils.exceptions.product_exceptions import InsufficientStockException
+
+
+async def create_order(
+        profile_id: UUID,
+        order_items: list[IOrderItemCreate],
+        db_session: AsyncSession
+) -> IOrderRead:
+    if not order_items:
+        raise EmptyOrderItemsException()
+    async with db_session.begin():
+        try:
+            new_order = Order(
+                profile_id=profile_id,
+                status=IOrderStatus.processing,
+                total_amount=0
+            )
+            db_session.add(new_order)
+            await db_session.flush()
+
+            for item in order_items:
+                product = await crud.product.fetch_one(id=item.product_id, db_session=db_session)
+                if not product:
+                    raise IdNotFoundException(model=Product, id=item.product_id)
+                if not product.is_available_for(item.quantity):
+                    raise InsufficientStockException(product.id)
+                product.stock_quantity -= item.quantity
+
+                new_item = OrderItem(
+                    product_id=product.id,
+                    order_id=new_order.id,
+                    quantity=item.quantity,
+                    unit_price=product.price,
+                )
+                new_order.total_amount += product.price * item.quantity
+                db_session.add(new_item)
+                db_session.add(product)
+
+            await db_session.commit()
+            return new_order
+        except SQLAlchemyError as e:
+            await db_session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
+            )
+        finally:
+            await db_session.close()
