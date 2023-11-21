4504dd32f5bc61ea959fa2a4c9f9820c98f27f0f
denis-kosobanov <kosobanovden@mail.ru>
2023-11-21 20:21:09 +0500

    Refactor image create

--- app/api/v1/endpoints/image.py
+++ app/api/v1/endpoints/image.py
@@ -1,23 +1,31 @@
 from fastapi import UploadFile, APIRouter, status, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import FileResponse
 
 from app import crud
+from app.api.deps import get_db_session
 from app.dependencies.image_deps import image_type_existing
 from app.schemas.image_schema import IImageRead
+from app.services.image_service import get_file_path_dir, create_image
 
 router = APIRouter()
 
 
 @router.post('/uploads', status_code=status.HTTP_201_CREATED)
 async def post_image(
-        file: UploadFile = Depends(image_type_existing)
+        file: UploadFile = Depends(image_type_existing),
+        db_session: AsyncSession = Depends(get_db_session)
 ) -> IImageRead:
-    return await crud.image.create(file=file)
+    return await create_image(file=file, db_session=db_session)
 
 
 @router.get("/{file_name}")
 async def get_image(file_name: str):
     image = await crud.image.fetch_one(name=file_name)
+    file_path = get_file_path_dir(
+        upload_dir="app/static/images/",
+        file_name=file_name,
+    )
     if not image:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Image {file_name} not found')
-    return FileResponse(image.path, headers={f"Content-Type": f"image/{image.format}"})
+    return FileResponse(file_path, headers={f"Content-Type": f"image/{image.format}"})
--- app/crud/image_crud.py
+++ app/crud/image_crud.py
@@ -1,47 +1,9 @@
-import datetime
-import hashlib
-import os
-import shutil
-
-from fastapi import UploadFile
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.crud.crud_base import CRUDBase
 from app.models import Image
 
 
 class CRUDImage(CRUDBase[Image]):
-    async def create(
-            self,
-            *,
-            file: UploadFile,
-            db_session: AsyncSession | None = None,
-    ) -> Image:
-        db_session = db_session or self.get_db().session
-
-        file_format = file.filename.split(".")[-1]
-
-        hasher = hashlib.sha256()
-        file_name = str(datetime.datetime.utcnow()) + file.filename
-        hasher.update(file_name.encode("utf-8"))
-        file_hash = hasher.hexdigest()
-
-        depth = 2
-        subdirectory = os.path.join(*[file_hash[i:i + depth] for i in range(0, depth, depth)])
-        upload_dir = "app/static/images/"
-        file_path = os.path.normpath(os.path.join(upload_dir, subdirectory, file_hash))
-
-        os.makedirs(os.path.join(upload_dir, subdirectory), exist_ok=True)
-
-        with open(file_path, "wb") as dest_file:
-            shutil.copyfileobj(file.file, dest_file)
-
-        image = Image(path=file_path, format=file_format, name=file_hash[:11])
-
-        db_session.add(image)
-        await db_session.commit()
-        await db_session.refresh(image)
-        return image
+    pass
 
 
 image = CRUDImage(Image)
