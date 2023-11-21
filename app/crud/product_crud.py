38b244915713e10573a81ebb53cbf601e6556928
denis-kosobanov <kosobanovden@mail.ru>
2023-11-21 20:22:00 +0500

    Update update_image

--- app/crud/product_crud.py
+++ app/crud/product_crud.py
@@ -1,9 +1,9 @@
 from fastapi import UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app import crud
 from app.crud.crud_base import CRUDBase
 from app.models import Product, Image
+from app.services.image_service import create_image
 
 
 class CRUDProduct(CRUDBase[Product]):
@@ -15,7 +15,7 @@ class CRUDProduct(CRUDBase[Product]):
             db_session: AsyncSession | None = None,
     ) -> Product:
         db_session = db_session or self.get_db().session
-        image: Image = await crud.image.create(file=file)
+        image: Image = await create_image(file=file, db_session=db_session)
         product.image = image
         db_session.add(product)
         await db_session.commit()
