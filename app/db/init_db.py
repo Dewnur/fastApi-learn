1fa0bf465cf3c299b5d534eeec44d2e33bb58db2
denis-kosobanov <kosobanovden@mail.ru>
2023-11-28 01:55:30 +0500

    Refactor init db

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -60,62 +60,6 @@ users: list[dict[str, str | IUserAccess | IProfileCreate]] = [
 ]
 
 
-# categories: list[ICategoryCreate] = [
-#     ICategoryCreate(name='Electronics'),
-#     ICategoryCreate(name='Clothes'),
-#     ICategoryCreate(name='Furniture'),
-# ]
-#
-# products = [
-#     {
-#         'data': IProductCreate(
-#             name='Canon EOS 650D',
-#             description='Зеркальная камера',
-#             price=31_472,
-#             stock_quantity=20,
-#         ),
-#         'category': 'Electronics',
-#         'image': './data/canon.jpg',
-#     },
-#     {
-#         'data': IProductCreate(
-#             name='Black Fox B10 Fox 32 ГБ голубой',
-#             description='product main image ядер - 4x(1.4 ГГц),'
-#                         ' 2 ГБ, 2 SIM, IPS, 1600x720, камера 13'
-#                         ' Мп, NFC, 4G, GPS, FM, 4000 мА*ч',
-#             price=5_599,
-#             stock_quantity=50,
-#         ),
-#         'category': 'Electronics',
-#         'image': './data/phone.jpg',
-#     },
-#     {
-#         'data': IProductCreate(
-#             name='Кровать Bella',
-#             description='Современная мягкая компактная кровать '
-#                         'Белла с большим количеством вариантов '
-#                         'обивки дает вам возможность вписать ее '
-#                         'в любой интерьер на который хватит вашей'
-#                         ' фантазии.',
-#             price=11_999,
-#             stock_quantity=5,
-#         ),
-#         'category': 'Electronics',
-#         'image': './data/bed.jpg',
-#     },
-#     {
-#         'data': IProductCreate(
-#             name='Ferz',
-#             description='Шапка',
-#             price=1254,
-#             stock_quantity=34,
-#         ),
-#         'category': 'Electronics',
-#         'image': './data/head.jpg',
-#     },
-# ]
-
-
 async def init_db(async_session: AsyncSession):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
@@ -135,23 +79,12 @@ async def init_db(async_session: AsyncSession):
             profile.user_id = new_user.id
             await crud.profile.create(obj=profile, db_session=async_session)
 
-    # for category in categories:
-    #     await crud.category.create(obj=category, db_session=async_session)
-
-    # for product in products:
-    #     category = await crud.category.fetch_one(name=product['category'], db_session=async_session)
-    #     new_product_create = product['data']
-    #     new_product_create.category_id = category.id
-    #     new_product = await crud.product.create(obj=new_product_create, db_session=async_session)
-    #     with open(product['image'], "rb") as file:
-    #         upload_file = UploadFile(file=file, filename='canon.jpg')
-    #         await crud.product.update_image(file=upload_file, product=new_product, db_session=async_session)
-
     def open_mock_json(model: str):
         with open(f"./data/{model}.json", encoding="utf-8") as file:
             return json.load(file)
 
     categories = open_mock_json("categories")
+    products = open_mock_json("products")
 
     for Model, values in [
         (Category, categories),
@@ -160,8 +93,6 @@ async def init_db(async_session: AsyncSession):
         await async_session.execute(query)
 
     await async_session.commit()
-
-    products = open_mock_json("products")
 
     for item in products:
         new_product_create = IProductCreate(**item['product'])
