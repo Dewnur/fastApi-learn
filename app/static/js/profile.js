167e02ab2e7e0d501a9cd5cc069098d9a21abd01
denis-kosobanov <kosobanovden@mail.ru>
2023-11-30 03:32:16 +0500

    Update fields

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -1,17 +1,26 @@
 const field_values = []
 
-function hide_edit_btn() {
-    hide("btn-edit-profile")
-    hide("btn-save")
-    hide("btn-cancel")
+function toggleDisplay(element) {
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
 }
 
-function hide(id) {
-    const toBeHidden = document.getElementById(id);
-    if (toBeHidden.style.display === "none") {
-        toBeHidden.style.display = "block";
+function toggleReadonly(element) {
+    if (element.hasAttribute("readonly")) {
+        element.removeAttribute("readonly", "true");
     } else {
-        toBeHidden.style.display = "none";
+        element.setAttribute("readonly", "true");
+    }
+}
+
+function hide_edit_btn() {
+    const edit_btn_id_list = ["btn-edit-profile", "btn-save", "btn-cancel"]
+    for (const id in edit_btn_id_list) {
+        const element = document.getElementById(edit_btn_id_list[id])
+        toggleDisplay(element)
     }
 }
 
@@ -20,12 +29,16 @@ function open_edit_profile() {
     const input_fields = document.getElementsByName("profile-input")
     input_fields.forEach((element) => field_values.push(element.value))
     input_fields.forEach((element) => element.className = "form-control")
+    input_fields.forEach((element) => toggleReadonly(element))
+
 }
 
 function cancel_edit() {
     hide_edit_btn()
     const input_fields = document.getElementsByName("profile-input")
     input_fields.forEach((element) => element.className = "form-control-plaintext")
+    input_fields.forEach((element) => toggleReadonly(element))
+
     for (let i = 0; i < input_fields.length; i++) {
         input_fields[i].value = field_values[i]
     }
